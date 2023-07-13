d11c47038ac764bab136e579f0938e94e66725e9
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-13 17:34:23 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+process.env.CI = false
 ReactDOM.render( 
   <React.StrictMode>
     <App />
