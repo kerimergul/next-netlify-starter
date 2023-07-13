99f62855c1420d83aea93a3d2946404c0247bda4
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-13 17:01:08 +0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+ReactDOM.render( 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,  
+  document.getElementById('root')
+);
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ pages/screens/screen2.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./screen1.css";
+const style = { backgroundImage: 'url(/backgrounds/screen2_1440.jpg)' };
+
+class Screen1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div class="screen1-body"
+        style={style}>
+      </div>
+    );
+
+  }
+}
+export default Screen1;
