6c3c64d18a2b14b3c8689b8826fff6b632d0b2e2
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-24 15:13:27 +0300

    commit

--- src/uploadScreen.js
+++ src/uploadScreen.js
@@ -22,9 +22,9 @@ class UploadScreen extends Component {
         })
     }
 
-    async uploadToServer(data) {
+    async uploadToServer(img) {
         console.log(['uploadToServer', data])
-        axios.post("https://signal-server.onrender.com/api/image/getImage", { img: data }).then((res) => {
+        axios.post("https://signal-server.onrender.com/api/image/upload", { img }).then((res) => {
             if (res.data.status === true) {
                 alert("Resim başarıyla yüklendi");
             } else {
