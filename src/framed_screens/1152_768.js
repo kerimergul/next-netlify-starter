32cc0c7605d40b02894c868f4d6cce9460efff2d
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-25 14:29:15 +0300

    commit

---
+++ src/framed_screens/1152_768.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./style.css";
+import axios from "axios";
+
+const style = { backgroundImage: 'url(/backgrounds/bg_1152_768.jpg)' };
+
+class _1152_768 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            img: false,
+            counter: 0,
+            index: 0
+        };
+    }
+
+    componentDidMount() {
+        this.getImg();
+    }
+
+    getImg() {
+        let skip = 0;
+        axios.post("https://signal-server.onrender.com/api/image/getImage", { skip }).then((res) => {
+            if (res?.data?.status === true) {
+                this.setState({
+                    img: res.data.img,
+                })
+            } else {
+                alert('Resim yüklenirken hata oluştu')
+            }
+        }).catch((err) => {
+            alert("Resim yüklenirken hata oluştu");
+            console.log(err);
+        })
+    }
+
+    componentWillUnmount() { }
+
+
+    renderImg(img) {
+        let data = `${img?.data}`.replace('"', '').replace('"', '');
+        return <img src={data} alt="image_480" class="i"></img>
+    }
+
+    render() {
+        return (
+            <div class="_1152_768"
+                style={style}>
+                {this.state.img !== false ? this.renderImg(this.state.img) : <div></div>}
+            </div>
+        );
+
+    }
+}
+export default _1152_768;
---
+++ src/framed_screens/864_432.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./style.css";
+import axios from "axios";
+
+const style = { backgroundImage: 'url(/backgrounds/bg_864_432.jpg)' };
+
+class _864_432 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            img: false,
+            counter: 0,
+            index: 0
+        };
+    }
+
+    componentDidMount() {
+        this.getImg();
+    }
+
+    getImg() {
+        let skip = 0;
+        axios.post("https://signal-server.onrender.com/api/image/getImage", { skip }).then((res) => {
+            if (res?.data?.status === true) {
+                this.setState({
+                    img: res.data.img,
+                })
+            } else {
+                alert('Resim yüklenirken hata oluştu')
+            }
+        }).catch((err) => {
+            alert("Resim yüklenirken hata oluştu");
+            console.log(err);
+        })
+    }
+
+    componentWillUnmount() { }
+
+
+    renderImg(img) {
+        let data = `${img?.data}`.replace('"', '').replace('"', '');
+        return <img src={data} alt="image_480" class="i"></img>
+    }
+
+    render() {
+        return (
+            <div class="_864_432"
+                style={style}>
+                {this.state.img !== false ? this.renderImg(this.state.img) : <div></div>}
+            </div>
+        );
+
+    }
+}
+export default _864_432;
