20ca1153e40df5f7de02e465661f90c065e7af04
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-31 17:26:35 +0300

    commit

---
+++ src/framed_screens/4092_960_2.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "./style.css";
+import axios from "axios";
+
+const style = { backgroundImage: 'url(/backgrounds/bg_4092_960.jpg)' };
+
+class _4092_960 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            img: false,
+            skip: 0,
+            first: true,
+        };
+    }
+
+    componentDidMount() {
+        if (this.state.first === true) {
+            this.getImg();
+        }
+        this.interval = setInterval(async () => {
+            this.getImg();
+        }, 12000)
+    }
+
+    getImg() {
+        let skip = 0;
+        axios.post("https://signal-server.onrender.com/api/image/getImage", { skip }).then((res) => {
+            if (res?.data?.status === true) {
+                this.setState({
+                    img: res.data.img,
+                    skip: skip + 1,
+                    first: false
+                })
+            } else {
+                alert('Resim yüklenirken hata oluştu')
+            }
+        }).catch((err) => {
+            alert("Resim yüklenirken hata oluştu");
+            console.log(err);
+        })
+    }
+
+    componentWillUnmount() { clearInterval(this.interval) }
+
+
+    renderImg(img) {
+        let data = `${img?.data}`.replace('"', '').replace('"', '');
+        return <img src={data} alt="image_480" class="i_landscape_v2"></img>
+    }
+
+    render() {
+        return (
+            <div class="_4092_960_v2"
+                style={style}>
+                {this.state.img !== false ? this.renderImg(this.state.img) : <div></div>}
+            </div>
+        );
+
+    }
+}
+export default _4092_960;
