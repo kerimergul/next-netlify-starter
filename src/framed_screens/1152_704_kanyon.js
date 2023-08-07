7c7a6080656a281b8dca3e047928e7c81255c3c2
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-08-08 02:10:59 +0300

    Revert "commit"
    This reverts commit e23fb412d8aa4b89afbc087b781c49e9e298e3df.

--- src/framed_screens/1152_704_kanyon.js
+++ src/framed_screens/1152_704_kanyon.js
@@ -8,47 +8,39 @@ class _1152_704_Kanyon extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            img: `../images/0-min.png`,
-            skip: Math.floor(Math.random() * 74),
+            img: false,
+            skip: 0,
             first: true,
         };
     }
+
     componentDidMount() {
-        // if (this.state.first === true) {
-        //     this.getImg();
-        // }
+        if (this.state.first === true) {
+            this.getImg();
+        }
         this.interval = setInterval(async () => {
             this.getImg();
-        }, 8000)
+        }, 12000)
     }
 
     getImg() {
         let skip = this.state.skip;
-        if (skip > 74) {
-            skip = 0;
-        }
-        // axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
-        //     if (res?.data?.status === true) {
-        let path = `../images/${this.state.skip}.png`;
-        this.setState({
-            img: path,
-            skip: skip + 1,
-            first: false
+        axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
+            if (res?.data?.status === true) {
+                this.setState({
+                    img: res.data.img,
+                    skip: skip + 1,
+                    first: false
+                })
+            } else {
+                alert('Resim yüklenirken hata oluştu')
+            }
+        }).catch((err) => {
+            alert("Resim yüklenirken hata oluştu");
+            console.log(err);
         })
-        // } else {
-        //     alert('Resim yüklenirken hata oluştu')
-        // }
-        // }).catch((err) => {
-        //     alert("Resim yüklenirken hata oluştu");
-        //     console.log(err);
-        // })
     }
 
-    renderImg(img) {
-        // let data = `${img?.data}`.replace('"', '').replace('"', '');
-        let data = img;
-        return <img id='img' src={data} alt="image_480" class="i_v1"  ></img>
-    }
     componentWillUnmount() { clearInterval(this.interval) }
 
     setImgHeigth() {
@@ -61,10 +53,10 @@ class _1152_704_Kanyon extends Component {
             resim.style.width = '55%';
         }
     }
-    // renderImg(img) {
-    //     let data = `${img?.data}`.replace('"', '').replace('"', '');
-    //     return <img id="img" src={data} alt="image_480" class="i_landscape_v1" onLoad={this.setImgHeigth}></img>
-    // }
+    renderImg(img) {
+        let data = `${img?.data}`.replace('"', '').replace('"', '');
+        return <img id="img" src={data} alt="image_480" class="i_landscape_v1" onLoad={this.setImgHeigth}></img>
+    }
 
     render() {
         return (
--- src/framed_screens/4092_960_1.js
+++ src/framed_screens/4092_960_1.js
@@ -8,54 +8,49 @@ class _4092_960_v1 extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            img: `../images/0-min.png`,
-            skip: Math.floor(Math.random() * 74),
+            img: false,
+            skip: 0,
             first: true,
         };
     }
+
     componentDidMount() {
-        // if (this.state.first === true) {
-        //     this.getImg();
-        // }
+        if (this.state.first === true) {
+            this.getImg();
+        }
         this.interval = setInterval(async () => {
             this.getImg();
-        }, 8000)
+        }, 12000)
     }
 
     getImg() {
         let skip = this.state.skip;
-        if (skip > 74) {
+        if (skip > 10) {
             skip = 0;
         }
-        // axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
-        //     if (res?.data?.status === true) {
-        let path = `../images/${this.state.skip}.png`;
-        this.setState({
-            img: path,
-            skip: skip + 1,
-            first: false
+        axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
+            if (res?.data?.status === true) {
+                this.setState({
+                    img: res.data.img,
+                    skip: skip + 1,
+                    first: false
+                })
+            } else {
+                alert('Resim yüklenirken hata oluştu')
+            }
+        }).catch((err) => {
+            alert("Resim yüklenirken hata oluştu");
+            console.log(err);
         })
-        // } else {
-        //     alert('Resim yüklenirken hata oluştu')
-        // }
-        // }).catch((err) => {
-        //     alert("Resim yüklenirken hata oluştu");
-        //     console.log(err);
-        // })
     }
 
-    renderImg(img) {
-        // let data = `${img?.data}`.replace('"', '').replace('"', '');
-        let data = img;
-        return <img id='img' src={data} alt="image_480" class="i_v1"  ></img>
-    }
     componentWillUnmount() { clearInterval(this.interval) }
 
 
-    // renderImg(img) {
-    //     let data = `${img?.data}`.replace('"', '').replace('"', '');
-    //     return <img src={data} alt="image_480" class="i_landscape_v2"></img>
-    // }
+    renderImg(img) {
+        let data = `${img?.data}`.replace('"', '').replace('"', '');
+        return <img src={data} alt="image_480" class="i_landscape_v2"></img>
+    }
 
     render() {
         return (
