c0479fdc9b0474e2fca7f9d104a86e30ef6e5d53
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-08-07 14:19:47 +0300

    commit

--- src/framed_screens/2160_3840_bagdat.js
+++ src/framed_screens/2160_3840_bagdat.js
@@ -25,7 +25,7 @@ class _2160_3840_Bagdat extends Component {
 
     getImg() {
         let skip = this.state.skip;
-        axios.post("https://www.tesvik-sgk.com/signal/api/image/getImageBagdat", { skip }).then((res) => {
+        axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
             if (res?.data?.status === true) {
                 this.setState({
                     img: res.data.img,
