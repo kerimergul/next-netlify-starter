0b63f5cb4f99d08eb8a7cb45ad5cc813d759bcd7
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-08-08 00:54:51 +0300

    commit

--- src/framed_screens/1080_1920.js
+++ src/framed_screens/1080_1920.js
@@ -25,23 +25,24 @@ class _1080_1920 extends Component {
 
     getImg() {
         let skip = this.state.skip;
-        if (skip > 10) {
+        if (skip > 74) {
             skip = 0;
         }
-        axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
-            if (res?.data?.status === true) {
-                this.setState({
-                    img: res.data.img,
-                    skip: skip + 1,
-                    first: false
-                })
-            } else {
-                alert('Resim yüklenirken hata oluştu')
-            }
-        }).catch((err) => {
-            alert("Resim yüklenirken hata oluştu");
-            console.log(err);
+        // axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
+        //     if (res?.data?.status === true) {
+        let path = `../images/${this.state.skip}.png`;
+        this.setState({
+            img: path,
+            skip: skip + 1,
+            first: false
         })
+        // } else {
+        //     alert('Resim yüklenirken hata oluştu')
+        // }
+        // }).catch((err) => {
+        //     alert("Resim yüklenirken hata oluştu");
+        //     console.log(err);
+        // })
     }
 
     componentWillUnmount() { clearInterval(this.interval) }
@@ -52,11 +53,12 @@ class _1080_1920 extends Component {
 
         if (resimGenislik < sayfaGenislik) {
             resim.style.width = '100%';
-        } 
+        }
     }
 
     renderImg(img) {
-        let data = `${img?.data}`.replace('"', '').replace('"', '');
+        // let data = `${img?.data}`.replace('"', '').replace('"', '');
+        let data = img;
         return <img id='img' src={data} alt="image_480" class="i_v1" onLoad={this.setImageWidth} ></img>
     }
 
