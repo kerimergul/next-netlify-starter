355580f34766eeb027c8bfe734b6129a6a46a0fb
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-10-24 11:14:41 +0300

    commit

---
+++ src/framed_screens/2160_3840.akbank.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import "./style.css";
+import axios from "axios";
+
+const style = { backgroundImage: 'url(/backgrounds/bg_2160_3840-min.jpg)' };
+
+class _2160_3840 extends Component {
+    constructor(props) {
+        super(props);
+        let skip = Math.floor(Math.random() * 6)
+        this.state = {
+            img: `../images/${skip}-min.webp`,
+            skip: Math.floor(Math.random() * 6),
+            first: true,
+        };
+    }
+
+    componentDidMount() {
+        // if (this.state.first === true) {
+        //     this.getImg();
+        // }
+        this.interval = setInterval(async () => {
+            this.getImg();
+        }, 6000)
+    }
+
+    getImg() {
+        let skip = this.state.skip;
+        // if (skip > 74) {
+        //     skip = 0;
+        // }
+        // axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
+        //     if (res?.data?.status === true) {
+            let path = `../images/${skip}-min.webp`;
+        skip = Math.floor(Math.random() * 6)
+        this.setState({
+            img: path,
+            skip: skip,
+            first: false
+        })
+        // } else {
+        //     alert('Resim yüklenirken hata oluştu')
+        // }
+        // }).catch((err) => {
+        //     alert("Resim yüklenirken hata oluştu");
+        //     console.log(err);
+        // })
+    }
+
+    componentWillUnmount() { clearInterval(this.interval) }
+
+    setImageWidth() {
+        const resim = document.getElementById('img');
+        const sayfaGenislik = window.innerWidth;
+        const resimGenislik = resim.clientWidth;
+
+        if (resimGenislik < sayfaGenislik) {
+            resim.style.width = '100%';
+        }
+    }
+
+
+
+    renderImg(img) {
+        // let data = `${img?.data}`.replace('"', '').replace('"', '');
+        let data = img;
+        return <img id='img' src={data} alt="image_480" class="i_v1" onLoad={this.setImageWidth}  ></img>
+    }
+
+    render() {
+        return (
+            <div class="bg_vertical"
+                style={style}>
+                {this.state.img !== false ? this.renderImg(this.state.img) : <div></div>}
+            </div>
+        );
+
+    }
+}
+export default _2160_3840;
---
+++ src/framed_screens/384_960.akbank.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import "./style.css";
+import axios from "axios";
+
+const style = { backgroundImage: 'url(/backgrounds/bg_384_960-min.jpg)' };
+
+class _384_960 extends Component {
+    constructor(props) {
+        super(props);
+        let skip = Math.floor(Math.random() * 6)
+        this.state = {
+            img: `../images/${skip}-min.webp`,
+            skip: Math.floor(Math.random() * 6),
+            first: true,
+        };
+    }
+
+    componentDidMount() {
+        // if (this.state.first === true) {
+        //     this.getImg();
+        // }
+        this.interval = setInterval(async () => {
+            this.getImg();
+        }, 6000)
+    }
+
+    getImg() {
+        let skip = this.state.skip;
+        // if (skip > 74) {
+        //     skip = 0;
+        // }
+        // axios.post("https://www.tesvik-sgk.com/signal/api/image/getImage", { skip }).then((res) => {
+        //     if (res?.data?.status === true) {
+        let path = `../images/${skip}-min.webp`;
+        skip = Math.floor(Math.random() * 6);
+        this.setState({
+            img: path,
+            skip: skip,
+            first: false
+        })
+        // } else {
+        //     alert('Resim yüklenirken hata oluştu')
+        // }
+        // }).catch((err) => {
+        //     alert("Resim yüklenirken hata oluştu");
+        //     console.log(err);
+        // })
+    }
+    componentWillUnmount() { clearInterval(this.interval) }
+
+    setImageWidth() {
+        const resim = document.getElementById('img');
+        const sayfaGenislik = window.innerWidth;
+        const resimGenislik = resim.clientWidth;
+
+        if (resimGenislik < sayfaGenislik) {
+            resim.style.height = '72%';
+            resim.style.maxHeight = '72%';
+            resim.style.top = '35%';
+        }
+    }
+
+    renderImg(img) {
+        // let data = `${img?.data}`.replace('"', '').replace('"', '');
+        let data = img;
+        return <img id='img' src={data} alt="image_480" class="i_v1"  ></img>
+    }
+
+    render() {
+        return (
+            <div class="bg_vertical"
+                style={style}>
+                {this.state.img !== false ? this.renderImg(this.state.img) : <div></div>}
+            </div>
+        );
+
+    }
+}
+export default _384_960;
